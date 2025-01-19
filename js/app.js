128076da513ab66aeada176c2cf0e485b66b607f
Daniel Pelstring <danielpelstring@macbookpro.lan>
2025-01-19 18:49:58 -0500

    Got the script to generate a randome number between 1 and 100

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
+// computer generates a random Number
+// user enters a guess
+// if the guess = the Number, user wins
+// if the guess does not = the guess user goes again, a nax of two times
+
+
 "use strict";
+
+const guessElement = document.getElementById("guess-message");
+const currentElement = document.getElementById("current-guess");
+const computerElement = document.getElementById("computer-guess");
+const historyElement = document.getElementById("guess-history");
+const inputNumElement = document.getElementById("guess-inpu")
+
+// Number Generation/Computer Guess
+function generatedNumber (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//Guess Function
+
+
+
+ function render() {
+    // Call the created functions
+    generatedNumber (0, 100);
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
+
+let clear = document.getElementById("restart-btn")
+clear.addEventListener("click", function(){
+  expression = "0"
+  displayValue()
+})
